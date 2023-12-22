83bca4eca7c22473d0a2500899c87ff3869ef38a
LeonardoDelacruz18 <113712078+LeonardoDLC18@users.noreply.github.com>
2023-12-21 22:54:33 -0500

    Add files via upload

---
+++ WpfProyecto1/Clases/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace WpfProyecto1.Clases
+{
+    public class Conexion
+    {
+
+        private readonly string nombreDB = "logeodb";
+        private readonly string usuarioDB = "bbdad0w85pf32sdb36g4";
+        private readonly string contraDB = "pscale_pw_6fEQAq0cqRR2UaNSUAAGip6R8gfMzUkARiZTvb3ngwN";
+        private readonly string server = "aws.connect.psdb.cloud";
+        private readonly string sslMode = "VerifyFull"; // Cambiar según la configuración de la base de datos
+        private readonly string connectionString;
+
+        private MySqlConnection cx;
+
+        public Conexion()
+        {
+            connectionString = $"Server={server};Database={nombreDB};" +
+                $"User ID={usuarioDB};Password={contraDB};SslMode={sslMode}";
+        }
+
+        public MySqlConnection Conectar()
+        {
+            try
+            {
+                cx = new MySqlConnection(connectionString);
+                cx.Open();
+                Console.WriteLine("Se conectó");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se conectó");
+                Console.WriteLine(ex.Message);
+            }
+            return cx;
+        }
+
+        public void Desconectar()
+        {
+            try
+            {
+                cx.Close();
+                Console.WriteLine("Se desconectó");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ WpfProyecto1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfProyecto1.Clases;
+
+namespace WpfProyecto1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Conexion conexion;
+        public MainWindow()
+        {
+            InitializeComponent();
+            conexion = new Conexion();
+        }
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contra = txtContraseña.Text;
+
+            // Abrir una conexión utilizando la clase de conexión
+            using (MySqlConnection connection = conexion.Conectar())
+            {
+                // Verificar que la conexión no sea nula
+                if (connection != null)
+                {
+                    // Ejemplo de consulta SELECT
+                    string consultaSql = $"SELECT * FROM Usuarios WHERE usuario= '{usuario}' AND contra='{contra}'";
+
+                    // Crear un comando SQL utilizando la consulta y la conexión
+                    using (MySqlCommand command = new MySqlCommand(consultaSql, connection))
+                    {
+                        // Ejecutar la consulta y obtener un lector de datos
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                MessageBox.Show("Se inició SESION");
+                            }
+                            else
+                            {
+                                MessageBox.Show("NO se inició SESION");
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Desconectar al salir del bloque using para liberar recursos
+            conexion.Desconectar();
+        }
+    }
+}
